3da20051ea6ef3081f3605322c8abcf49ed744cd
unknown <kucingplaygame@gmail.com>
2021-11-04 20:38:05 +0700

    first commit

---
+++ get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+    <form action="https://getform.io/f/3d95c992-22c2-41f4-9546-b8ad08958ba5" method="POST">
+        <input type="text" name="name">
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
